c87930a840946a61c4e78c8e143747164d6dbef4
Anuj0124Kumar <anuj0214@gmail.com>
2023-03-12 13:56:31 +0530

    try

--- script.js
+++ script.js
@@ -13,10 +13,16 @@ let image = document.getElementById('image');
 let text = document.querySelector(".text");
 
 async function fetchData() {
+  if(place.value === ""){
+    alert("Input can't be empty");
+    return;
+  }
+  document.getElementById("error").style.display = "none";
   document.getElementById("hidden").style.display = "block";
   document.getElementById("initial").style.display = "none";
   setTimeout(async () => {
     document.getElementById("hidden").style.display = "none";
+    try {
     const url = `https://api.weatherapi.com/v1/current.json?key=53e35489bace4ae591b180749231003&q=${place.value}`;
     const data = await fetch(url);
     const parseData = await data.json();
@@ -34,5 +40,8 @@ async function fetchData() {
     precipitate.innerHTML = `Precipitate : ${parseData.current.precip_mm} `;
     humidity.innerHTML = `Humidity : ${parseData.current.humidity}% `;
     document.getElementById("initial").style.display = "block";
+    } catch (error) {
+      document.getElementById("error").style.display = "block";
+    }
   }, 2000);
 }
