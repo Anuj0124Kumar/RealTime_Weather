a214cdfd29d7178f07cc6d1bba880bc5a46205d7
Anuj0124Kumar <anuj0214@gmail.com>
2023-03-12 14:43:36 +0530

    title

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ let wind = document.querySelector(".wind");
 let pressure = document.querySelector(".pressure");
 let precipitate = document.querySelector(".precipitate");
 let humidity = document.querySelector(".humidity");
-let image = document.getElementById('image');
+let image = document.getElementById("image");
 let text = document.querySelector(".text");
 
 async function fetchData() {
-  if(place.value === ""){
+  if (place.value === "") {
     alert("Input can't be empty");
     return;
   }
@@ -23,23 +23,23 @@ async function fetchData() {
   setTimeout(async () => {
     document.getElementById("hidden").style.display = "none";
     try {
-    const url = `https://api.weatherapi.com/v1/current.json?key=53e35489bace4ae591b180749231003&q=${place.value}`;
-    const data = await fetch(url);
-    const parseData = await data.json();
-    console.log(parseData.location.name);
-    city.innerHTML = `${parseData.location.name}, `;
-    state.innerHTML = `${parseData.location.region}, `;
-    country.innerHTML = `${parseData.location.country} `;
-    last_upadated.innerHTML = `Last updated : ${parseData.current.last_updated} `;
-    temp.innerHTML = `Temperature : ${parseData.current.temp_c} <sup>o</sup>C `;
-    faren.innerHTML = `Fahrenheit : ${parseData.current.temp_f} <sup>o</sup>F`;
-    image.src = `${parseData.current.condition.icon}`;
-    text.innerHTML =`${parseData.current.condition.text}`
-    wind.innerHTML = `Wind (km/h) : ${parseData.current.wind_kph} `;
-    pressure.innerHTML = ` Pressure(mb): ${parseData.current.pressure_mb} `;
-    precipitate.innerHTML = `Precipitate : ${parseData.current.precip_mm} `;
-    humidity.innerHTML = `Humidity : ${parseData.current.humidity}% `;
-    document.getElementById("initial").style.display = "block";
+      const url = `https://api.weatherapi.com/v1/current.json?key=53e35489bace4ae591b180749231003&q=${place.value}`;
+      const data = await fetch(url);
+      const parseData = await data.json();
+      console.log(parseData.location.name);
+      city.innerHTML = `${parseData.location.name}, `;
+      state.innerHTML = `${parseData.location.region}, `;
+      country.innerHTML = `${parseData.location.country} `;
+      last_upadated.innerHTML = `Last updated : ${parseData.current.last_updated} `;
+      temp.innerHTML = `Temperature : ${parseData.current.temp_c} <sup>o</sup>C `;
+      faren.innerHTML = `Fahrenheit : ${parseData.current.temp_f} <sup>o</sup>F`;
+      image.src = `${parseData.current.condition.icon}`;
+      text.innerHTML = `${parseData.current.condition.text}`;
+      wind.innerHTML = `Wind (km/h) : ${parseData.current.wind_kph} `;
+      pressure.innerHTML = ` Pressure(mb): ${parseData.current.pressure_mb} `;
+      precipitate.innerHTML = `Precipitate : ${parseData.current.precip_mm} `;
+      humidity.innerHTML = `Humidity : ${parseData.current.humidity}% `;
+      document.getElementById("initial").style.display = "block";
     } catch (error) {
       document.getElementById("error").style.display = "block";
     }
